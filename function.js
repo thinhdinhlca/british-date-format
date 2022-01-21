308ba767a5876caeb06d20ca5816ed4aca0cdeac
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-01-21 13:10:24 +0000

    Update function.js

--- function.js
+++ function.js
@@ -6,7 +6,7 @@ day = day.value ?? "";
 hour = hour.value ?? "";
 minute = minute.value ?? "";
 
-const s = new Date(year, month-1, day, hour, minute, 0, 0).replace(/-/g, '/').replace('T', ' ');
+const s = new Date(year, month-1, day, hour, minute, 0, 0).replace(/-/g, '/');
 
 return s.toLocaleString("en-AU", {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true}).toString();
 
